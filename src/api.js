2fa1d764a6889ab796ae4f135e425b1c6b79b0c0
unknown <deepanshu.17220@knit.ac.in>
2023-04-04 13:33:28 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const serverless= require('serverless-http');
+
+const app= express();
+
+const router = express.Router();
+
+
+router.get('/', (req, res)=>{
+    res.status(200).json({
+        'hi': "hellow"
+    });
+})
+
+router.get('/text', (req, res)=>{
+    res.status(200).json({
+        'text': "hellow"
+    });
+})
+app.use('/.netlify/functions/api' ,router);
+
+module.exports.handler = serverless(app);
+
